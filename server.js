21269f395b0b493882226edb514cdf89090e5cb3
shafeeka <shafnis123@gmail.com>
2023-10-09 12:51:02 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const expressLayouts = require("express-ejs-layouts")
+
+const port = 5000;
+const indexRouter = require('./routes/index')
+
+app.set('view engine', "ejs")
+app.set('views', __dirname + "/views")
+app.set('layout','layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// mongodb password = Q9HgqyiR33RzqO6S
+
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect('mongodb+srv://shafnis123:Q9HgqyiR33RzqO6S@cluster0.lospciv.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((error) => {
+    console.error('MongoDB Connection Error:', error);
+  });
+
+const db = mongoose.connection;
+
+
+
+// const schema = {
+//   type: "object",
+//   properties: {
+//     TaskName: {type: "string"},
+//     TaskGroup: {type: "string"},
+//   },
+//   required: ["TaskName","TaskGroup"],
+//   additionalProperties: false
+// }
+
+app.use('/', indexRouter)
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
+
+
